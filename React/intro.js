d29328829f1ac2150717cc7bc2fff423d3d57013
Ehtasham <ehtasham8292@gmail.com>
2023-10-21 21:42:06 +0530

    js mini project paused

---
+++ React/intro.js
@@ -0,0 +1,14 @@
+// React is a JS library
+// React is all about component 
+// component => it can be called function of react , reusable piece of code 
+// SPA approach => Single Page Application 
+// Dynamic Change 
+// Alternatives of React => Angular , Vue ;
+
+
+// Why React ?
+// Reusibility
+// Readibility
+// Separation Of Concern
+// Maintainibility
+
